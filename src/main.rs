448070f0c70d7891bad2e2288339ea62e76325b5
Toshiki <wata.hal.student@gmail.com>
2025-01-10 16:16:06 +0900

    エラーメッセージ修正

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,20 @@ use green::lexical_analyzer;
 use green::parser;
 use green::interpreter;
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let config = match cli_arg_parse::Config::new(&args) {
         Ok(config) => config,
-        Err(e) => {
-            eprintln!("{}", e);
-            return;
-        }
+        Err(e) => return Err(e),
     };
     
     let content = match utils::load_file_content(&config.file_path) {
         Ok(content) => content,
         Err(_) => {
             match utils::get_error_message("FILE001", &[("file_name", &config.file_path)]) {
-                Ok(message) => eprintln!("{}", message),
-                Err(message) => eprintln!("{}", message),
+                Ok(message) => return Err(message),
+                Err(message) => return Err(message),
             }
-            return;
         }
     };
 
@@ -31,10 +27,9 @@ fn main() {
         Ok(tokens) => tokens,
         Err(e) => {
             match utils::get_error_message("LEX001", &[("message", &e)]) {
-                Ok(message) => eprintln!("{}", message),
-                Err(message) => eprintln!("{}", message),
+                Ok(message) => return Err(message),
+                Err(message) => return Err(message),
             }
-            return;
         }
     };
 
@@ -42,20 +37,20 @@ fn main() {
         Ok(node) => node,
         Err(e) => {
             match utils::get_error_message("PARSE001", &[("message", &e)]) {
-                Ok(message) => eprintln!("{}", message),
-                Err(message) => eprintln!("{}", message),
+                Ok(message) => return Err(message),
+                Err(message) => return Err(message),
             }
-            return;
         }
     };
 
     if config.option == cli_arg_parse::RunMode::Execute {
         if let Err(e) = interpreter::execute(&ast) {
             match utils::get_error_message("RUNTIME001", &[("message", &e)]) {
-                Ok(message) => eprintln!("{}", message),
-                Err(message) => eprintln!("{}", message),
+                Ok(message) => return Err(message),
+                Err(message) => return Err(message),
             }
-            return;
         }
     }
+
+    return Ok(())
 }
